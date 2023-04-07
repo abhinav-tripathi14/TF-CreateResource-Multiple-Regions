06b0703a30c917a963f2f6ea47d7d941c65ca1b5
Abhinav Tripathi <47190942+abhinav-tripathi14@users.noreply.github.com>
2023-04-07 17:06:51 +0530

    Update outputs.tf

--- Module/outputs.tf
+++ Module/outputs.tf
@@ -1,12 +1,12 @@
 output "machine1ipaddress" {
-  value = module.createresourcetworegions.machine1ipaddress
+  value = aws_instance.serveruseast.public_ip
 }
 output "machine1iami" {
-  value = module.createresourcetworegions.machine1iami
+  value = aws_instance.serveruseast.ami
 }
 output "machine2ipaddress" {
-  value = module.createresourcetworegions.machine2ipaddress
+  value = aws_instance.serveruswest.public_ip
 }
 output "machine2ami" {
-  value = module.createresourcetworegions.machine2ami
+  value = aws_instance.serveruswest.ami
 }
