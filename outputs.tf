32b6778aeda8cca1312b6e65c96349470c411004
Abhinav Tripathi <47190942+abhinav-tripathi14@users.noreply.github.com>
2023-04-07 17:07:59 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "machine1ipaddress" {
+  value = module.createresourcetworegions.machine1ipaddress
+}
+output "machine1iami" {
+  value = module.createresourcetworegions.machine1iami
+}
+output "machine2ipaddress" {
+  value = module.createresourcetworegions.machine2ipaddress
+}
+output "machine2ami" {
+  value = module.createresourcetworegions.machine2ami
+}
