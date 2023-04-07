7dabcd22c098cdc78628b0c98bc5b525dfb79f20
Abhinav Tripathi <47190942+abhinav-tripathi14@users.noreply.github.com>
2023-04-07 17:10:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 module "createresourcetworegions" {
   source = "./ModuleExample"    
-  
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
