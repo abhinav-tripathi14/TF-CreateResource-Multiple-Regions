7519aaad1f80cb9e3b4ea6db287e00d7740d765f
Abhinav Tripathi <47190942+abhinav-tripathi14@users.noreply.github.com>
2023-04-07 17:07:28 +0530

    Create variables.tf

---
+++ Module/variables.tf
@@ -0,0 +1,8 @@
+variable "region1" {
+  default = "us-east-1"
+  description = "First Region where resource is to be created"
+}
+variable "region2" {
+  default = "us-west-1"
+  description = "Second Region where resource is to be created"
+}
